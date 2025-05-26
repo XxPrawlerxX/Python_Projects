f5a3ce9b7d26f0521aa8a9c4239f60f7ab021caf
XxPrawlerxX <121575092+XxPrawlerxX@users.noreply.github.com>
2025-05-26 18:47:21 +0100

    nother thing learnt

--- test file.py
+++ test file.py
@@ -1,43 +1,54 @@
 
 
-prices= {"apple": 10, "orange":20, "thing":12}
-cart={"apple":8, "orange":9}
-total_cost = 0
-for key, value in cart.items():
-    total_cost += value * prices[key]
+# prices= {"apple": 10, "orange":20, "thing":12}
+# cart={"apple":8, "orange":9}
+# total_cost = 0
+# for key, value in cart.items():
+#     total_cost += value * prices[key]
 
-print(total_cost)
-
-
-
-
-word = "banana"
-
-letter_count= {}
-
-for i in word:
-    if i in letter_count:
-        letter_count[i] += 1
-
-    else:
-        letter_count[i] = 1
-
-print(letter_count)
+# print(total_cost)
 
 
 
 
-num1 = input("Enter a number: ")
-num2 = input("Enter a number: ")
-try:
-    x = int(num1) / int(num2)
-except ZeroDivisionError:
-    print("Cannot divide by zero")
-    x = None
-except Exception as e:
-    print("Error occured")
-    x = None
-finally:
-    print(x)
+# word = "banana"
+
+# letter_count= {}
+
+# for i in word:
+#     if i in letter_count:
+#         letter_count[i] += 1
+
+#     else:
+#         letter_count[i] = 1
+
+# print(letter_count)
+
+
+
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter a number: ")
+# try:
+#     x = int(num1) / int(num2)
+# except ZeroDivisionError:
+#     print("Cannot divide by zero")
+#     x = None
+# except Exception as e:
+#     print("Error occured")
+#     x = None
+# finally:
+#     print(x)
     
 
+myFile = open("data.txt", "r")
+
+print(myFile.read(5))
+myFile.seek(0)
+print(myFile.read(5))
+
+myFile.close()
+
+myFile2 = open("data2.txt", "w")
+myFile2.write("helloworld")
+myFile2.close()
